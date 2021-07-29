c97708759477db194cf0dc3ef7efa2fcc5e0179d
p.aghasian <p.aghasian@myagah.com>
2021-07-29 15:30:58 +0430

    make product-serive hybrid

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const microservice = app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      // urls: ['amqp://user:password@localhost:5672/'],
+      urls: [
+        'amqps://fpweywyq:zXcspMIEbbmmYefPOtzr5a1N11OUF1Ry@hummingbird.rmq.cloudamqp.com/fpweywyq',
+      ],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  app.enableCors();
+  await app.startAllMicroservices(() => {
+    console.log('Microservices is Listening');
+  });
   await app.listen(3001);
 }
 bootstrap();
