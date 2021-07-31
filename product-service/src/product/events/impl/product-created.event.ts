dda4a8b39530a380cc0fb6cada2e4349e6ce8b4f
Pedram Aghasian <aghasian@chargoon.net>
2021-07-31 15:41:36 +0430

    some tiny change

--- product-service/src/product/events/impl/product-created.event.ts
+++ product-service/src/product/events/impl/product-created.event.ts
@@ -2,5 +2,5 @@ import { CreateProductDto } from 'src/product/dtos/create-product.dto';
 import { IEvent } from '@nestjs/cqrs';
 
 export class ProductCreatedEvent implements IEvent {
-  constructor(private readonly product: CreateProductDto) {}
+  constructor(public readonly product: CreateProductDto) {}
 }
